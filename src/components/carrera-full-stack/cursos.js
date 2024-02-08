47d1e5f4da719a6866d680006886ea899fd90499
Jorge Emiliano Maldonado <jorgeemilianom@gmail.com>
2024-02-08 17:51:37 -0300

    add plan de carrera

---
+++ src/components/carrera-full-stack/cursos.js
@@ -0,0 +1,301 @@
+
+export const cursosFrontend = [
+    {
+        level: 'Fase 1: Fundamentos del Desarrollo Web',
+        data: [
+            {
+                title: '1. HTML:',
+                description: 'Aprende la estructura básica de las páginas web. Céntrate en etiquetas, atributos, y la semántica del HTML.',
+                curse: [
+                    {
+                        url_icon: 'https://w7.pngwing.com/pngs/954/559/png-transparent-html5-icon-%E2%80%A2-html-social-network-icon.png',
+                        name: 'Curso HTML',
+                        link: '#'
+                    }
+                ]
+            },
+            {
+                title: '2. CSS:',
+                description: 'Domina los estilos CSS para diseñar y dar formato a tu página web. Aprende sobre selectores, propiedades, box model, flexbox, y grid system.',
+                curse: [
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png',
+                        name: 'Curso CSS',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png',
+                        name: 'Flexbox en 15 min',
+                        link: '#'
+                    }
+                ]
+            },
+            {
+                title: '3. JavaScript (JS):',
+                description: 'Introdúcete al lenguaje de programación del web. Aprende sintaxis básica, tipos de datos, bucles, condicionales, funciones, y manipulación del DOM.',
+                curse: [
+                    {
+                        url_icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png',
+                        name: 'Curso Javascript',
+                        link: '#'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        level: 'Fase 2: Herramientas de Desarrollo y Control de Versiones',
+        data: [
+            {
+                title: '1. Herramientas de Desarrollo Web:',
+                description: 'Familiarízate con las herramientas de desarrollo de navegadores (Chrome DevTools, Firefox Developer Tools).',
+                curse: [
+                    {
+                        url_icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/768px-Google_Chrome_icon_%28September_2014%29.svg.png',
+                        name: 'Dev Tools',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png',
+                        name: 'Curso Visual Studio Code',
+                        link: '#'
+                    },
+                ]
+            },
+            {
+                title: '2. GIT:',
+                description: 'Git: Aprende a usar Git para el control de versiones de tus proyectos. Práctica con comandos básicos como git commit, git push, git pull, y git merge.',
+                curse: [
+                    {
+                        url_icon: 'https://cdn-icons-png.flaticon.com/512/4494/4494740.png',
+                        name: 'Curso GIT y GitHub',
+                        link: '#'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        level: 'Fase 3: Frameworks y Librerías',
+        data: [
+            {
+                title: '1. React.js:',
+                description: 'Aprende a crear interfaces de usuario dinámicas con React. Enfócate en componentes, estado, props, y el ciclo de vida de los componentes.',
+                curse: [
+                    {
+                        url_icon: 'https://www.svgrepo.com/show/452092/react.svg',
+                        name: 'Curso React',
+                        link: '#'
+                    }
+                ]
+            },
+            {
+                title: '2. Vue.js o Angular:',
+                description: 'Considera aprender un segundo framework o librería para entender diferentes enfoques en el desarrollo FrontEnd.',
+                curse: [
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_angular_icon_130754.png',
+                        name: 'Curso Angular',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://static-00.iconduck.com/assets.00/vue-icon-2048x1766-ntogpmti.png',
+                        name: 'Curso Vue',
+                        link: '#'
+                    },
+                ]
+            },
+            {
+                title: 'Sass o LESS: ',
+                description: 'Profundiza en preprocesadores CSS para escribir CSS de manera más eficiente.',
+                curse: [
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/lesscss_logo_icon_169274.png',
+                        name: 'Mini-curso LESS',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/2108/PNG/512/sass_icon_130835.png',
+                        name: 'Curso SASS',
+                        link: '#'
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        level: 'Fase 4: Responsive Design y Accesibilidad',
+        data: [
+            {
+                title: '1. Diseño Responsive:',
+                description: 'Asegura que tus sitios web se vean bien en todos los dispositivos. Aprende sobre media queries, unidades relativas, y frameworks de diseño responsive como Bootstrap.',
+                curse: [
+                    {
+                        url_icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png',
+                        name: 'Curso Bootstrap',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png',
+                        name: 'Mini-curso Creando web responsive',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png',
+                        name: 'Mini-curso Media queries',
+                        link: '#'
+                    },
+                ]
+            },
+            {
+                title: '2. Accesibilidad Web (a11y):',
+                description: 'Aprende las mejores prácticas para hacer tus sitios web accesibles para todos los usuarios, incluyendo aquellos con discapacidades.',
+                curse: [
+                    {
+                        url_icon: 'https://cdn-icons-png.flaticon.com/512/10838/10838348.png',
+                        name: 'Curso de Accesibilidad Web',
+                        link: '#'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        level: 'Fase 4: Responsive Design y Accesibilidad',
+        data: [
+            {
+                title: '1. Diseño Responsive:',
+                description: 'Asegura que tus sitios web se vean bien en todos los dispositivos. Aprende sobre media queries, unidades relativas, y frameworks de diseño responsive como Bootstrap.',
+                curse: [
+                    {
+                        url_icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png',
+                        name: 'Curso Bootstrap',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png',
+                        name: 'Mini-curso Creando web responsive',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png',
+                        name: 'Mini-curso Media queries',
+                        link: '#'
+                    },
+                ]
+            },
+            {
+                title: '2. Accesibilidad Web (a11y):',
+                description: 'Aprende las mejores prácticas para hacer tus sitios web accesibles para todos los usuarios, incluyendo aquellos con discapacidades.',
+                curse: [
+                    {
+                        url_icon: 'https://cdn-icons-png.flaticon.com/512/10838/10838348.png',
+                        name: 'Curso de Accesibilidad Web',
+                        link: '#'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        level: 'Fase 5: Herramientas Modernas de Desarrollo',
+        data: [
+            {
+                title: '1. NPM/Yarn:',
+                description: 'Gestiona las dependencias de tus proyectos con gestores de paquetes.',
+                curse: [
+                    {
+                        url_icon: 'https://static-00.iconduck.com/assets.00/npm-icon-2048x2048-3xqnd60o.png',
+                        name: 'Mini-curso NPM',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://static-00.iconduck.com/assets.00/yarn-icon-1024x1024-sdi5tb8x.png',
+                        name: 'Mini-curso Crear proyecto con YARN',
+                        link: '#'
+                    },
+                ]
+            },
+            {
+                title: '2. Webpack/Babel:',
+                description: 'Aprende sobre la transpilación de JS y el empaquetado de módulos para optimizar tus aplicaciones.',
+                curse: [
+                    {
+                        url_icon: 'https://static-00.iconduck.com/assets.00/webpack-plain-icon-461x512-3atd00pk.png',
+                        name: 'Curso WebPack',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://user-images.githubusercontent.com/3025322/87547253-bf050400-c6a2-11ea-950a-280311bc6cc8.png',
+                        name: 'Mini-curso Babel + WebPack',
+                        link: '#'
+                    }
+                ]
+            },
+            {
+                title: '3. ES6 y más allá:',
+                description: 'Mantente al día con las últimas características de JavaScript.',
+                curse: [
+                    {
+                        url_icon: 'https://play-lh.googleusercontent.com/fGJsYUru3pnsXJyfel6RSTYeckP4Dw2Ccy3RGHnJ5vdoui2C3GWy9Qc5NrYBLs8PLl_t',
+                        name: 'Mini-curso Javascript modeno ES6',
+                        link: '#'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        level: 'Fase 6: Proyectos Prácticos',
+        data: [
+            {
+                title: '1. Construye proyectos personales:',
+                description: 'Aplica todo lo aprendido construyendo tus propios proyectos. Algunas ideas incluyen un portafolio personal, una aplicación de lista de tareas, o un clon de una página web existente.',
+                curse: [
+                    {
+                        url_icon: 'https://i.pinimg.com/originals/98/71/a2/9871a2b4d286efb7ec1264267e062530.png',
+                        name: 'Portfolio con HTML, CSS y Js',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://i.pinimg.com/originals/98/71/a2/9871a2b4d286efb7ec1264267e062530.png',
+                        name: 'Portfolio con React',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://i.pinimg.com/originals/98/71/a2/9871a2b4d286efb7ec1264267e062530.png',
+                        name: 'Portfolio con React+Vite',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://i.pinimg.com/originals/98/71/a2/9871a2b4d286efb7ec1264267e062530.png',
+                        name: 'CRUD con Javascript',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://i.pinimg.com/originals/98/71/a2/9871a2b4d286efb7ec1264267e062530.png',
+                        name: 'CRUD con React + API',
+                        link: '#'
+                    },
+                    {
+                        url_icon: 'https://i.pinimg.com/originals/98/71/a2/9871a2b4d286efb7ec1264267e062530.png',
+                        name: 'Ecommerce con React',
+                        link: '#'
+                    },
+                    
+                ]
+            },
+            {
+                title: '2. Contribuye a proyectos de código abierto:',
+                description: 'Mejora tus habilidades y tu CV contribuyendo a proyectos reales.',
+                curse: [
+                    {
+                        url_icon: 'https://i.pinimg.com/originals/98/71/a2/9871a2b4d286efb7ec1264267e062530.png',
+                        name: 'Mini-curso Crear CV para desarrollador',
+                        link: '#'
+                    }
+                ]
+            },
+        ]
+    },
+];
